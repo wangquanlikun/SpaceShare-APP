70ad1a76b9c0bcc22cfee1c9b90c5497a542786c
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2024-02-20 22:04:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function changeContent(page) {
     if(page == 1)
         document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/rent.htm" style="width:100%; height:780px;"></object>';
     else if(page == 2)
-        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/whatsup.htm" ></object>';
+        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/whatsup.htm" style="width:100%; height:780px;"></object>';
     else if(page == 3)
-        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/my.htm" ></object>';
+        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/my.htm" style="width:100%; height:780px;"></object>';
 }
