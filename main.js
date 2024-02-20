74258b8a7ee60dc0ac576f5e86c3affffef86d0f
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2024-02-20 17:55:21 +0800

    完善了借出空间的前端页面

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function changeContent(page) {
     if(page == 1)
-        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/rent.htm" style="width:100%; height:720px;"></object>';
+        document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/rent.htm" style="width:100%; height:780px;"></object>';
     else if(page == 2)
         document.getElementById("content").innerHTML = '<object type="text/html" data="Pages/whatsup.htm" ></object>';
     else if(page == 3)
